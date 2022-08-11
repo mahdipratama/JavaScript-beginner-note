a3cab67fb0de4e65186904cfbd57957578e1ff13
mahdipratama <mahdipratama29@gmail.com>
2022-08-11 18:54:31 +0700

    JavaScript DOM crash course part 2

--- dom.js
+++ dom.js
@@ -90,16 +90,108 @@
 // lastItem.style.color = 'blue';
 
 
-let titles = document.querySelectorAll('.title');
+// let titles = document.querySelectorAll('.title');
 
-console.log(titles);
-titles[0].textContent = 'Masukan Item';
+// console.log(titles);
+// titles[0].textContent = 'Masukan Item';
 
-let ganjil = document.querySelectorAll('li:nth-child(odd)');
-let genap = document.querySelectorAll('li:nth-child(even)');
+// let ganjil = document.querySelectorAll('li:nth-child(odd)');
+// let genap = document.querySelectorAll('li:nth-child(even)');
 
 
-for (let i = 0; i < ganjil.length; i++) {
-  ganjil[i].style.backgroundColor = '#f4f4f4'
-  genap[i].style.backgroundColor = '#ccc'
-}
+// for (let i = 0; i < ganjil.length; i++) {
+//   ganjil[i].style.backgroundColor = '#f4f4f4'
+//   genap[i].style.backgroundColor = '#ccc'
+// }
+
+
+
+// TRAVERSING THE DOM //
+
+let itemList = document.querySelector('#items');
+// //parentNode
+
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = '#f4f4f4'
+// console.log(itemList.parentNode.parentNode.parentNode);
+
+
+// parentElement //
+
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = '#f4f4f4'
+// console.log(itemList.parentElement.parentElement.parentNode)
+
+
+// childNodes (Not Suggested by brad) //
+
+// console.log(itemList.childNodes);
+
+
+// children (Suggested by Brad) //
+
+// console.log(itemList.children);
+// console.log(itemList.children[2]);
+// itemList.children[2].style.backgroundColor = 'lightBlue'
+
+
+// FirstChild (Not Suggested by Brad) //
+
+// console.log(itemList.firstChild);
+
+
+// first Element Child (Suggested by Brad) //
+
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello there!'
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello there!'
+
+
+// nextSibling (Not Suggested by Brad) //
+// console.log(itemList.nextSibling);
+
+
+// nextELementSibling (Suggested by Brad) //
+// console.log(itemList.nextElementSibling);
+
+
+// previousSibling (Not suggested by Brad) //
+// console.log(itemList.previousSibling);
+
+
+// previousElementSibling (Suggested by Brad) //
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'limegreen'
+// itemList.previousElementSibling.style.textTransform = 'uppercase'
+
+
+// Create Element //
+
+// create a div //
+let newDiv = document.createElement('div');
+
+// Add class //
+newDiv.className = 'hello'
+
+// Add id //
+newDiv.id = 'hello1'
+
+// Add attr
+newDiv.setAttribute('title', 'Hello Div');
+
+// Create text node //
+let newDivText = document.createTextNode('Hello World');
+
+// Add text to div //
+newDiv.appendChild(newDivText);
+
+let container = document.querySelector('header .container')
+let h1 = document.querySelector('header h1')
+
+console.log(newDiv);
+
+newDiv.style.fontSize = '1.5em';
+
+container.insertBefore(newDiv, h1);
+
