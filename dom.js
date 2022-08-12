415b51445cb97fafee699ec78cf3605fb05e704d
mahdipratama <mahdipratama29@gmail.com>
2022-08-12 13:32:15 +0700

    JavaScript DOM crash course part 3

--- dom.js
+++ dom.js
@@ -106,6 +106,11 @@
 
 
 
+//---------------------------PART 2----------------------
+
+
+
+
 // TRAVERSING THE DOM //
 
 let itemList = document.querySelector('#items');
@@ -168,30 +173,118 @@ let itemList = document.querySelector('#items');
 
 // Create Element //
 
-// create a div //
-let newDiv = document.createElement('div');
+// // create a div //
+// let newDiv = document.createElement('div');
 
-// Add class //
-newDiv.className = 'hello'
+// // Add class //
+// newDiv.className = 'hello'
 
-// Add id //
-newDiv.id = 'hello1'
+// // Add id //
+// newDiv.id = 'hello1'
 
-// Add attr
-newDiv.setAttribute('title', 'Hello Div');
+// // Add attr
+// newDiv.setAttribute('title', 'Hello Div');
 
-// Create text node //
-let newDivText = document.createTextNode('Hello World');
+// // Create text node //
+// let newDivText = document.createTextNode('Hello World');
 
-// Add text to div //
-newDiv.appendChild(newDivText);
+// // Add text to div //
+// newDiv.appendChild(newDivText);
 
-let container = document.querySelector('header .container')
-let h1 = document.querySelector('header h1')
+// let container = document.querySelector('header .container')
+// let h1 = document.querySelector('header h1')
 
-console.log(newDiv);
+// console.log(newDiv);
 
-newDiv.style.fontSize = '1.5em';
+// newDiv.style.fontSize = '1.5em';
 
-container.insertBefore(newDiv, h1);
+// container.insertBefore(newDiv, h1);
+
+
+
+//---------------------------PART 3----------------------
+
+// EVENTS // 
+
+// let button = document.getElementById('button').addEventListener('click', buttonClick)
+
+// function buttonClick(e) {
+//   //console.log('Button Clicked');
+//   // document.getElementById('header-title').textContent = 'Items Entered';
+//   // document.querySelector('#main').style.backgroundColor = '#f4f4f4'
+//   // console.log(e);
+
+//   // console.log(e.target);
+//   // console.log(e.target.id);
+//   // console.log(e.target.className);
+//   // console.log(e.target.classList); 
+//   // let output = document.getElementById('output');
+//   // output.innerHTML = `<h3> ${e.target.id} </h3>`
+
+//   // console.log(e.type);
+//   // console.log(e.clientX);
+//   // console.log(e.clientY);
+
+//   // console.log(e.offsetX);
+//   // console.log(e.offsetY);
+
+//   //   console.log(e.altKey);
+//   //   console.log(e.shiftKey);
+//   //   console.log(e.ctrlKey);
+// }
+
+let button = document.getElementById('button'); 
+let box = document.getElementById('box'); 
+
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mouseup', runEvent);
+// button.addEventListener('mousedown', runEvent);
+
+// box.addEventListener('mouseenter', runEvent)
+// box.addEventListener('mouseleave', runEvent)
+
+// box.addEventListener('mouseover', runEvent)
+// box.addEventListener('mouseout', runEvent)
+
+// box.addEventListener('mousemove', runEvent)
+
+let itemInput = document.querySelector('input[type="text"]');
+let form = document.querySelector('form');
+let select = document.querySelector('select');
+
+// itemInput.addEventListener('keydown', runEvent)
+// itemInput.addEventListener('keyup', runEvent)
+// itemInput.addEventListener('keypress', runEvent)
+
+// itemInput.addEventListener('focus', runEvent)
+// itemInput.addEventListener('blur', runEvent)
+
+// itemInput.addEventListener('cut', runEvent)
+// itemInput.addEventListener('paste', runEvent)
+
+// itemInput.addEventListener('input', runEvent)
+
+// select.addEventListener('change', runEvent)
+// select.addEventListener('input', runEvent)
+
+form.addEventListener('submit', runEvent);
+
+
+function runEvent(e) {
+  e.preventDefault();
+  console.log(`EVENT TYPE: ${e.type}`);
+
+  
+  // console.log(e.target.value);
+
+
+  // console.log(e.target.value);
+  // document.getElementById('output').innerHTML = `<h3>${e.target.value}</h3>`
+
+  // output.innerHTML = `<h3>MouseX: ${e.offsetX} </h3> <h3>MouseY: ${e.offsetY} </h3>`
+
+  // box.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)"
+  // document.body.style.backgroundColor = "rgb("+e.offsetX+", "+e.offsetY+", 40)"
+}
 
